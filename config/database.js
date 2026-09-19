4cc17fa70cd5304e39136d84a5a8aefda5d062cd
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-09-19 16:23:56 +0200

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const createConnection = (uri) => {
+    
+  if (!uri) {
+    console.error('Error: La URI de la base de datos no está definida.');
+    process.exit(1);
+  }
+
+  const connection = mongoose.createConnection(uri);
+
+
+  connection.on('connected', () => {
+    console.log(` Instancia de base de datos conectada correctamente.`);
+  });
+
+  connection.on('error', (err) => {
+    console.error(` Error en la conexión a la base de datos: ${err.message}`);
+  });
+
+  connection.on('disconnected', () => {
+    console.log(' Conexión a la base de datos cerrada.');
+  });
+
+
+  return connection;
+};
+
+module.exports = createConnection;
